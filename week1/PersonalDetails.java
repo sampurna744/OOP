e627a5ae66a7ac0c19564f85b305bc3b21fbb2bc
sampurna simkhada <sampurnasimkhada@gmail.com>
2024-03-11 11:41:49 +0545

    personaldetail.java

---
+++ week1/PersonalDetails.java
@@ -0,0 +1,15 @@
+// 3. Create a second project called “PersonalDetails” and write a program that outputs some personal details to the screen, e.g. Name, Address, Age, Phone number etc.
+
+public class PersonalDetails {
+    public static void main(String args[]) {
+        String name = "sampurna simkhada";
+        int age = 21;
+        long phone = 9823237444L; // Use long for large numbers and suffix with 'L'
+        String address = "kathmandu";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(phone);
+        System.out.println(address);
+    }
+}
