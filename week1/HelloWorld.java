70cf758dc20ddce3c927c19d81db1edd013913bb
sampurna simkhada <sampurnasimkhada@gmail.com>
2024-03-11 11:31:43 +0545

    helloworld.java

---
+++ week1/HelloWorld.java
@@ -0,0 +1,13 @@
+// 1. Create a new project called “HelloWorld” and write a simple Java program that displays the message “Hello World” to the console.
+
+public class HelloWorld 
+{
+
+ public static void main(String args[])
+{
+ System.out.println("Hello World"); 
+ 
+ 
+}
+
+}
