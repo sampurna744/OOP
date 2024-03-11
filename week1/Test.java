ba6b293b857058f8cd494dd5b8059241b57266f8
sampurna simkhada <sampurnasimkhada@gmail.com>
2024-03-11 11:51:11 +0545

    test.java

---
+++ week1/Test.java
@@ -0,0 +1,31 @@
+// 2. “Test”. Create a project, enter, compile and run the following application.
+
+public class Test
+{
+   //----------------------------------------------------
+   //  Prints a statement.
+   //----------------------------------------------------
+   public static void main(String[] args)
+   {
+      System.out.println ("An Emergency Broadcast");
+   }
+
+   }
+
+
+
+/* a.change Test to test:Error: Could not find or load main class test */
+
+/* b. Change Emergency to emergency:output-An emergency Broadcast */
+
+
+/*c.  Remove the first quotation mark in the string: error: unclosed string literal
+    System.out.println (An Emergency Broadcast");*/
+/*d. Change main to man.:error Change main to man.*/
+
+/*e. Change println to bogus. error:  cannot find symbol*/
+
+/*f.  Remove the semicolon at the end of the println statement.error :unclosed comment*/
+
+/*g. Remove the last brace in the program. error:unclosed comment */
+
